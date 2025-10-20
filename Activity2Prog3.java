aab539ce71b7ce53064acdbcd2d1ae99302bd40e
Jenmarc Ronquillo <jenmarcxronquillo@gmail.com>
2025-10-20 13:30:52 +0800

    Fixed a mistake.

--- Activity2Prog3.java
+++ Activity2Prog3.java
@@ -3,18 +3,18 @@ import java.util.Scanner;
 public class Activity2Prog3 {
 
   
-    public static void main(String[] args) {
+    // public static void main(String[] args) {
         // Test 1: Fibonacci with start value 5 and range 50
-       System.out.println("Fibonacci Test: " + generateFibonacciSequence(5, 50));
+    //     System.out.println("Fibonacci Test: " + generateFibonacciSequence(5, 50));
 
     //     // Test 2: Factorial of 7
     //     System.out.println("Factorial Test: " + calculateFactorial(7));
         
     //     // Test 3: Grade for a score of 84
     //     System.out.println("Grade App Test: " + assignLetterGrade(84));
-     }
+    // }
 
-
+    
     /**
      * 1. Fibonacci Sequence App
      */
